347e3407fc53e0124bfe504492f26fb03a8843a6
f0x3s <byfoxes.design@gmail.com>
2026-06-18 10:57:43 -0500

    created fortune app

--- labs/02/p2.py
+++ labs/02/p2.py
@@ -5,3 +5,18 @@
 # python version 3.14.6
 # created 6/18/26 - foxes
 # modified 6/18/26 - foxes
+#
+# description: fortune cookie app - displays personaized fortunes for users
+
+# ask for user's name
+user_name = input("Please enter your name: ")
+
+# create a fortune message
+fortune = "A journey of a thousand miles begins with a single step."
+
+# display fortune message
+print("Your fortune cookie says: ")
+print(fortune)
+
+# create and display thank you message
+print("Thank you for using the Fortune Cookie App, " + user_name + "!")
