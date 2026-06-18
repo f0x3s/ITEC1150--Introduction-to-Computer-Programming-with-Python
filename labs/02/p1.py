1043d87150da6a4b4cf174035314ca4afc995b24
f0x3s <byfoxes.design@gmail.com>
2026-06-18 10:48:11 -0500

    created labs folder, began lab 02

---
+++ labs/02/p1.py
@@ -0,0 +1,31 @@
+# module 2 lab part 1
+# foxes
+# Instructor: Corin Dennison
+#
+# python version 3.14.6
+# created 6/18/26 - foxes
+# modified 6/18/26 - foxes
+
+age = 25            # integer
+pi = 3.14159        # float
+name = "Abdi"       # string
+is_student = True   # boolean
+
+# printing variables
+print("Age: ", age)
+print("Pi: ", pi)
+print("Name: ", name)
+print('Is Student?: ', is_student)
+
+# math operations
+a = 40
+b = 8
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
+
+# string concatenation
+first_name = "Abdi"
+last_name = ""
