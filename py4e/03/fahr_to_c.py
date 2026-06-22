a6528e84642ca12066940c74516b3a20672ec220
f0x3s <byfoxes.design@gmail.com>
2026-06-22 17:24:05 -0500

    f_to_c

--- py4e/03/fahr_to_c.py
+++ py4e/03/fahr_to_c.py
@@ -1,5 +1,8 @@
 inp = input('Enter temperature in fahrenheit: ')
-fahr = float(inp)
-cel = (fahr - 32.0) * 5.0 / 9.0
 
-print(cel)
+try: 
+    fahr = float(inp)
+    cel = (fahr - 32.0) * 5.0 / 9.0
+    print(cel)
+except: 
+    print('Please enter a Number')
