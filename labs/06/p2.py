b962a2bee016f629915d7fa5cc3f5bec342f0f2f
f0x3s <byfoxes.design@gmail.com>
2026-07-06 18:23:02 -0500

    input cleaning, polishing

--- labs/06/p2.py
+++ labs/06/p2.py
@@ -10,26 +10,34 @@
 
 CAPACITY = 9
 
-# adds new animal to input list as tuplet per element (<name>, <species>) and returns copy of list
-def add_animal(name, species, input_list) :
+# adds new animal to input list as tuple per element (<name>, <species>) and returns copy of list
+def add_animal(name, species, input_animals) :
     print(f"Adding: {name}, {species}...")
 
+    
     # basic cinput check to make sure user has entered both name and species.
     if (species is None or species == "") or (name is None or name == "") : 
-        print("ERROR: Could not add animal. Check name and species.")
+        print("\033[91mERROR: Could not add animal. Check name and species.\033[0m") # using ansi color escape codes for compatibility
 
     else :
+        try :
+            name = name.lower().replace(" ", "") # pulling double duty to ensure inputs are strings
+            species = species.lower().replace(" ", "")
 
-        # use get_animal_count() to check current # of animals against CAPACITY
-        if get_animal_count() < CAPACITY :
-            input_list.append((name, species))
-            print(f"Added {name} the {species}.")
-            print(f"Remaining capacity: {CAPACITY-get_animal_count()}")
-        else :
-            print(f"The shelter is at capacity, could not add {name} the {species}.")
-    
-    return list(input_list) # copy of input list.
+            # use get_animal_count() to check current # of animals against CAPACITY
+            if get_animal_count(input_animals) < CAPACITY :
+                input_animals.append((name, species))
+                print(f"Added {name.capitalize()} the {species}.")
+                print(f"Remaining capacity: {CAPACITY-get_animal_count(input_animals)}")
+            else :
+                print(f"The shelter is at capacity, could not add {name.capitalize()} the {species}.")
+            
+        except :
+            print("\033[91mERROR: Could not add animal. Expecting fomat: <string>, <string>.\033[0m") # using ansi color escape codes for compatibility
+     
+    return list(input_animals) # copy of input list.
 
+# return a list with each unique species (2nd element of input list's tuples)
 def available_species(available) :
     seen = []
 
@@ -40,9 +48,10 @@ def available_species(available) :
     
     return seen
 
-def adopt_animal(species) :
+def adopt_animal(species, input_animals) :
 
     pending_adoption = []
+    
 
     for individual in animals :
         if species == individual[1] :
@@ -50,9 +59,9 @@ def adopt_animal(species) :
     
     if len(pending_adoption) > 1 :
 
-        print(f"We have multiple {species}s:")
+        print(f"\nWe have multiple {plural(species)}:")
         for individual in pending_adoption :
-            print(individual[0])
+            print(individual[0].capitalize())
         
         print("\nWhich one would you like? Enter their name: ")
 
@@ -60,52 +69,74 @@ def adopt_animal(species) :
 
         while not is_exists :
 
-            requested_name = input().lower()
+            requested_name = input().replace(" ", "").lower()
 
             for individual in pending_adoption :
-                if requested_name == individual[0].lower() :
+                if requested_name == individual[0] :
                     is_exists = True
 
                 if is_exists : 
-                    print(f"you have adopted: {individual[0]}")
+                    print(f"you have adopted: {individual[0].capitalize()}")
                     animals.remove(individual)
                     break;
+            
+            if not is_exists :
+                print(f"We can't find {requested_name.capitalize()}. Please ensure correct spelling:")
     else :
-        print(f"You have adopted: {pending_adoption[0][0]} the {pending_adoption[0][1]}.")
+        print(f"You have adopted: {pending_adoption[0][0].capitalize()} the {pending_adoption[0][1]}.")
         animals.remove(pending_adoption[0])
 
 # returns number of animals in shelter
-def get_animal_count() :
-    return len(animals)
+def get_animal_count(input_animals) :
+    return len(input_animals)
 
+# specialty function for correcting irregular plurals
+def plural(animal_type) :
+    if animal_type == "mouse" :
+        animal_type = "mice"
+    else :
+        animal_type = animal_type + "s"
+
+    return animal_type 
 
 animals = [] # empty list to hold animals and species in shelter
 
-animals = add_animal("alice", "cat", animals)
-animals = add_animal("red", None, animals)
-animals = add_animal("bob", "dog", animals)
-animals = add_animal("angel", "cat", animals)
-animals = add_animal("zoey", "cat", animals)
+# test animals, mix of upper and lower case formatting and additional whitespace to simulate user input.
+animals = add_animal("Alice", "cat", animals)
+animals = add_animal("red", None, animals)  # incorrect input
+animals = add_animal("Bob", "dog", animals)
+animals = add_animal("angel", "Cat", animals)
+animals = add_animal("Zoey", "cat ", animals)
 animals = add_animal("spot", "dog", animals)
-animals = add_animal("zeke", "bird", animals)
-animals = add_animal("moxxy", "mouse", animals)
-animals = add_animal("nim", "mouse", animals)
-animals = add_animal("steven", "mouse", animals)
-animals = add_animal("poly", "bird", animals)
+animals = add_animal(30, 20, animals) # incorrect input
+animals = add_animal("Zeke", "Bird", animals)
+animals = add_animal("Moxxy ", "mouse", animals)
+animals = add_animal("Nim", "mouse ", animals)
+animals = add_animal("", "mouse", animals)
+animals = add_animal("Steven", "mouse", animals)
+animals = add_animal("poly", "Bird", animals)
 
-print(f"\nWeclome to the animal shelter! We have {get_animal_count()} animals. Current available types are: ")
+print(f"\nWeclome to the animal shelter! We have {get_animal_count(animals)} animals. Current available types are: ")
 
 # return unique species as list, unpack list and split with newlines
 print(*available_species(animals), sep="\n")
 
 while True :
     print("\nWhich species would you like?")
-    want_species = input().lower()
+    want_species = input()
 
-    if want_species.lower() not in available_species(animals) :
-        print(f"We don't have any {want_species}s.")
+    if want_species is None or want_species == "" : 
+        print("\033[91mERROR: Please enter species of desired animal: .\033[0m") # using ansi color escape codes for compatibility
+
     else :
-        break
+        want_species = want_species.lower().replace(" ", "") # input() always a string, no need to perform error checking like in add_animal() where we don't use input
+        
+        if want_species not in available_species(animals) :
+            print(f"We don't have any {want_species}s.")
+        else :
+            break
 
-adopt_animal(want_species)
+adopt_animal(want_species, animals)
+
+print()
 print(animals)
