22e9d5dfd40c72ffb4d4a11aff84610492c1d7b7
f0x3s <byfoxes.design@gmail.com>
2026-07-09 10:32:59 -0500

    finished p1.py

--- labs/08/p1.py
+++ labs/08/p1.py
@@ -8,11 +8,43 @@
 #
 # description: Object Oriented Programming (OOP)
 
-class Animal:
-    def __init__(self, name, age):
+class Animal :
+    def __init__(self, name, age) :
         self.name = name
         self.age = age 
-    def __str__(self):
+    def __str__(self) :
         return f"{self.name} is {self.age} years old"
-    def speak(self, sound):
+    def speak(self, sound) :
         return f"{self.name} says {sound}"
+    
+
+class Dog(Animal) :
+    def speak(self, sound = "Woof"):
+        return super().speak(sound)
+
+class Duck(Animal) :
+    def speak(self, sound = "Quack"):
+        return super().speak(sound)
+
+class Pig(Animal) :
+    def speak(self, sound = "Oink"):
+        return super().speak(sound)
+    
+class Cat(Animal) :
+    def speak(self, sound = "meow") :
+        return super().speak(sound)
+
+bob = Dog("Bob", 5)
+zoey = Cat("zoey", 14)
+winston = Dog("Winston", 12)
+loki = Dog("Loki", 4)
+delilah = Cat("Delilah", 2)
+howard = Duck("Howard", 52)
+babe = Pig("Babe", 30)
+
+menagerie = [bob, zoey, winston, loki, delilah, howard, babe]
+
+for creature in menagerie :
+    if isinstance(creature, Animal):
+        print(creature)
+        print(creature.speak())
