2131067fee42b32d7dad0a6613da0d6f0137ffa2
f0x3s <byfoxes.design@gmail.com>
2026-07-09 23:49:42 -0500

    finished rough

--- class-assignments/pm.py
+++ class-assignments/pm.py
@@ -7,64 +7,130 @@
 # modified 7/9/26 - foxes
 #
 # description: Python- based application to help customers select toppings for their tacos
+party_count = 0
 
-class Taco :
-    def __init__(self, tortilla, filling, toppings) :
-        self.tortilla = tortilla
-        self.filling = filling
-        self.toppings = toppings 
+MAX_TOPPING = 4
+MAX_DIP = 2
 
-print("Welcome to Catrinas Mexican Grill")
-
-MAX_TOPPINGS = 4
 human_numbers = ["first", "second", "third", "fourth", "fifth","sixth","seventh","eighth","ninth","tenth"]
 
 taco_options = {
     "tortilla" : (1, ["Corn", "Flour", "Whole Wheat"]),
     "filling" : (1, ["Beans", "Chicken", "Fish", "Beef"]),
-    "topping" : (MAX_TOPPINGS, ["Cheese", "Lettuce", "Tomatoes", "Onions", "Salsa", "Sour Cream"])
+    "topping(s)" : (MAX_TOPPING, ["Cheese", "Lettuce", "Tomatoes", "Onions", "Salsa", "Sour Cream"]),
+    "dip(s)" : (MAX_DIP, ["Red Sauce", "Green Sauce", "Queso Blanco", "Guacamole"])
 }
 
 def display_options(options, key) :
     for index, option in enumerate(options[key][1]) :
         print(f"{index + 1}. {option}")
 
-    print(f"{len(options[key][1])+1}. Done (no more {key}s)" if options[key][0] > 1 else "")
+    if options[key][0] > 1 :
+        print(f"{len(options[key][1]) + 1}. Done (no more {key}s)")
 
-def count_options(options) :
-    return len(options)
+def count_options(options, key) :
+    count = len(options[key][1]) 
+
+    return count + 1 if options[key][0] > 1 else count
 
 def human_number(number) :
     return human_numbers[number]
 
-print("Welcome to Catrinas!")
-print("Get ready to build your taco...")
+def is_done(options, key, selection) :
 
-for key in taco_options.keys() :
+    num_options = count_options(options, key)
+
+    if selection > num_options or selection < 1 :
+        raise ValueError("selection not in bounds")
+
+    return options[key][0] > 1 and selection == num_options
+
+def query_user(options) :
+    order = {}
+
+    for key in options.keys() :
    
-   num_items = taco_options[key][0]
-   for index in range(num_items) :
-       
-       str_index = "" if num_items == 1 else  human_number(index) + " "
+        order[key] = []
 
-       print(f"Select your {str_index}{key} (1-{count_options(taco_options[key][1])}):") 
-       display_options(taco_options, key)
-       
+        num_items = options[key][0]
+        done_flag = False
+
+        for index in range(num_items) :
+        
+            if done_flag :
+                print("done")
+                break
+        
+            str_index = "" if num_items == 1 else  human_number(index) + " "
+
+            print(f"\nSelect your {str_index}{key} (1-{count_options(options, key)}):") 
+            display_options(options, key)
+
+            while True :
+                user_input = input()
+                try :
+                    user_input = int(user_input)
+                    if(is_done(options, key, user_input)) :
+                            done_flag = True
+                            break
+                    
+                    user_input = options[key][1][user_input-1]
+
+                    order[key].append(user_input)
+                    break
+            
+                except :
+                    print(f"Error, expecting a number between 1 and {count_options(options, key)}:")
+    
+    return order
+
+def sanitize_party_count(count, max) :
+    count = int(count)
+    
+    if count < 1 or count > max:
+        raise ValueError("selection not in bounds")
+    return count
 
 
+print("Welcome to Catrinas Mexican Grill")
+print("Get ready to build your taco(s)...")
+max_guests = len(human_numbers)
 
+print(f"\nHow many are in your party? (maximum {max_guests} guests): ")
 
-
-
-#print(f"\nSelect yout tortilla type (1-{count_options(tortilla_types)}):")
-#display_options(tortilla_types)
-while True:
-    user_tortilla_type = input()[0:1]
+while True :
     try :
-        user_tortilla_type = int(user_tortilla_type)
-        user_tortilla_type = tortilla_types[user_tortilla_type]
+        party_count = sanitize_party_count(input(),max_guests)
         break
     except :
-        print(f"Error, expecting a number between 1 and {count_options(tortilla_types)}:")
+        print(f"Expecting an integer between 1 and {max_guests}:")
 
-print("Done")
+party_orders = []
+
+for order in range(party_count):
+
+    party_index = "W" if party_count == 1 else human_number(order).capitalize() + " party member, w"
+
+    print(f"\n{party_index}hat is your name?: ")
+    name = input()
+
+    party_orders.append({
+        "name": name,
+        "order": query_user(taco_options)
+    })
+
+print("\nOrder complete!\n")
+
+for member in party_orders:
+
+    print(f"{member['name']}:")
+
+    for key in member["order"]:
+
+        print(f"\t{key.capitalize()}: ", end="")
+
+        selections = ", ".join(member["order"][key]) if member["order"][key] else "None"
+        print(selections)
+
+        if "Salsa" in member["order"][key]:
+            print("\t\tOne Spicy Taco coming up!")
