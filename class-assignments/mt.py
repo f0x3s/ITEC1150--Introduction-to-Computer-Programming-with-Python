2a8c9ca2746a592280043cfcc45e252fbd4fefb1
f0x3s <byfoxes.design@gmail.com>
2026-07-14 15:31:15 -0500

    inital classes

--- class-assignments/mt.py
+++ class-assignments/mt.py
@@ -8,3 +8,32 @@
 #
 # description: Python-based application to help BTG customers order burgers.
 
+class Customer :
+    def __init__(self):
+        pass
+
+class Burger :
+    def __init__(self):
+        self.toppings = []
+        pass
+
+    def add_topping(self, topping) :
+        self.toppings.append(topping)
+
+TOPPINGS = {
+    "Lettuce 
+    "Tomatoes"
+    "Pickles"
+    "Grilled onions"
+    "Raw onions "
+    "Green peppers "
+    "Jalapeños"
+    "Mushrooms"
+    "Mayonnaise"
+    "Ketchup"
+    "Mustard"
+    "Sweet and sour relish"
+    "BBQ sauce"
+    "A1 Original Steak Sauce"
+    "Frank's Original Hot Sauce"
+    }
