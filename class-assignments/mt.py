0036bf7d761a370bb8ade0e1bdc8fbe45f2ab61f
f0x3s <byfoxes.design@gmail.com>
2026-07-14 15:13:38 -0500

    created midterm file

---
+++ class-assignments/mt.py
@@ -0,0 +1,10 @@
+# Midterm
+# foxes
+# Instructor: Corin Dennison
+#
+# python version 3.14.6
+# created 7/14/26 - foxes
+# modified 7/14/26 - foxes
+#
+# description: Python-based application to help BTG customers order burgers.
+
