c07fb42e7787916bc3061acc47c45f253a63a875
f0x3s <byfoxes.design@gmail.com>
2026-07-23 18:07:33 -0500

    naive recursive box blur

--- final-project/final.py
+++ final-project/final.py
@@ -21,11 +21,37 @@ def dicom_to_image (dicom_path) :
 def thresh_image(image, value) :
     thresh = image.copy()
 
-    for y, row in enumerate(thresh):
-        for x, pixel in enumerate(row):
+    for y, row in enumerate(thresh) :
+        for x, pixel in enumerate(row) :
             thresh[y][x] = 255 if pixel > value else 0
 
     return thresh
+
+def box_blur(image, level):
+    blur = image.copy()
+
+    for y, row in enumerate(image):
+        for x, pixel in enumerate(row):
+            pixel_sum = 0
+            count = 0
+
+            for nbr_y in [-1, 0, 1]:
+                for nbr_x in [-1, 0, 1]:
+                    local_y = y + nbr_y
+                    local_x = x + nbr_x
+
+                    if (0 <= local_y < image.shape[0] and 0 <= local_x < image.shape[1]):
+                        pixel_sum += int(image[local_y][local_x])
+                        count += 1
+
+            blur[y][x] = round(pixel_sum / count)
+
+    if level > 0 :
+        return box_blur(blur, level-1)
+    else :
+        return blur
+
+
 
 def main():
     dicom_path = "ct-lung-screening-nlst-instance.dcm"
@@ -37,6 +63,7 @@ def main():
     print(image.dtype) 
 
     image = thresh_image(image,127)
+    image = box_blur(image,10)
 
     cv2.imshow('Image Window', image)
     cv2.waitKey()
