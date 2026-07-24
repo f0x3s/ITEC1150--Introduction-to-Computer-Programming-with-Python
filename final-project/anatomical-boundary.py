ec78517290fd3c4291e0c2738f8ee07666a1c241
f0x3s <byfoxes.design@gmail.com>
2026-07-23 19:31:51 -0500

    bulk dicom processing

--- final-project/anatomical-boundary.py
+++ final-project/anatomical-boundary.py
@@ -1,14 +1,34 @@
 import os
+import dicom2jpg
 
 folder_path = "./lung-cancer-full-series"
 
 def list_files_in_folder(folder_path) :
 
+    dicom_files = []
+
     for item in os.scandir(folder_path) :
         if item.is_file() :
+
             root, extension = os.path.splitext(item)
             if extension == ".dcm" :
-                print(root)
+                file_name = root + extension
+                print("found: " + file_name)
+                dicom_files.append(file_name)
+    
+    return dicom_files
+
+def dicom_to_image(dicom_paths) :
+
+    images = []
+
+    for item in dicom_paths :
+        images.append(dicom2jpg.dicom2img(item))
+        print("processed: " + item)
+
+    return images
 
 
-list_files_in_folder(folder_path)
+series_files = list_files_in_folder(folder_path)
+
+series_images = dicom_to_image(series_files)
